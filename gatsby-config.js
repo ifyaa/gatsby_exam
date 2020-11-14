b83ba0dd08f1ad6fd3aa4c6296f2550b44fd78c4
ifyaa <ifyaa@sina.com>
2020-11-14 15:23:53 +0900

    first

---
+++ gatsby-config.js
@@ -0,0 +1,88 @@
+/* eslint-disable @typescript-eslint/camelcase */
+'use strict'
+
+module.exports = {
+  siteMetadata: {
+    title: 'Example',
+    description: 'This is an example site I made.',
+    siteUrl: 'https://example.com',
+    author: {
+      name: 'Jeff Rafter',
+      url: 'https://twitter.com/jeffrafter',
+      email: 'jeffrafter@gmail.com',
+    },
+    social: {
+      twitter: 'https://twitter.com/jeffrafter',
+      github: 'https://github.com/jeffrafter',
+    },
+  },
+  pathPrefix: "/gatsby_exam",
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/posts`,
+        name: `posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1280,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-autolink-headers`,
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://noyes.co.kr`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Jeff Rafter`,
+        short_name: `jeffrafter.com`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-typescript`,
+    `gatsby-transformer-sharp`,
+  ],
+}
